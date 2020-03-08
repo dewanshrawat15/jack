c351b9a982d26c0e3fe9786ecc13aa79cc23f150
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-03-08 20:06:01 +0530

    Updating app options
    Added an option to update volume settings

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,92 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int volLevel = 0;
+  TextEditingController volumeLevelController = TextEditingController();
+
+  void showAlertBox(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          "Volume Level",
+          style: TextStyle(
+            fontFamily: "Product Sans"
+          ),
+        ),
+        content: TextField(
+          autofocus: true,
+          textCapitalization: TextCapitalization.none,
+          controller: volumeLevelController,
+          onChanged: (val){
+            volLevel = int.parse(val);
+          },
+          textInputAction: TextInputAction.done,
+          cursorColor: Color(0xFF7E57C2),
+          style: TextStyle(
+            color: Color(0xFF7E57C2)
+          ),
+          decoration: InputDecoration(
+            enabledBorder: new UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFF7E57C2),
+                width: 1.0
+              ),
+            ),
+            focusedBorder: new UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFF7E57C2),
+                width: 1.0),
+            ),
+            icon: Icon(
+              Icons.volume_up,
+              color: Color(0xFF7E57C2)
+            ),
+            labelText: "Please enter a value from 0 - 10",
+            labelStyle: TextStyle(
+              color: Color(0xFF7E57C2),
+              fontFamily: "Product Sans"
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            highlightColor: Color(0x117E57C2),
+            splashColor: Color(0x117E57C2),
+            onPressed: (){
+              widget.client.execute(
+                'osascript -e "set Volume $volLevel"'
+              );
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Set Volume",
+              style: TextStyle(
+                fontFamily: "Product Sans",
+                color: Color(0xFF7E57C2)
+              ),
+            )
+          ),
+          FlatButton(
+            highlightColor: Color(0x117E57C2),
+            splashColor: Color(0x117E57C2),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Cancel",
+              style: TextStyle(
+                fontFamily: "Product Sans",
+                color: Color(0xFF7E57C2)
+              ),
+            )
+          )
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -144,6 +230,47 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
+
+              SizedBox(
+                height: 32,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: GestureDetector(
+                  onTap: () async{
+                    showAlertBox(context);
+                  },
+                  child: Card(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          height: MediaQuery.of(context).size.height / 5,
+                          width: MediaQuery.of(context).size.width - 48,
+                          color: Colors.indigoAccent,
+                        ),
+                        Positioned(
+                          left: 8,
+                          bottom: 10,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 20
+                            ),
+                            child: Text(
+                              "Set volume percentage",
+                              style: TextStyle(
+                                fontFamily: "Montserrat",
+                                fontSize: 20,
+                                color: Colors.white
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               SizedBox(
                 height: 48,
               )
@@ -152,7 +279,8 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () async{
+          await widget.client.disconnect();
           Navigator.pop(context);
         },
         child: Icon(Icons.power_settings_new),
